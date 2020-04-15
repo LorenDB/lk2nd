109a4324d5379d5d0360b0110f267cef66618f2b
Loren Burkholder <computersemiexpert@outlook.com>
2020-04-15 11:12:17 -0400

    Add changes for j3ltespr

--- dts/rules.mk
+++ dts/rules.mk
@@ -4,12 +4,13 @@ ifeq ($(PROJECT), msm8916-secondary)
 DTBS += \
 	$(LOCAL_DIR)/apq8016-samsung-r02.dtb \
 	$(LOCAL_DIR)/apq8016-samsung-r07.dtb \
+	$(LOCAL_DIR)/msm8916-longcheer-l8150.dtb \
 	$(LOCAL_DIR)/msm8916-motorola-harpia.dtb \
 	$(LOCAL_DIR)/msm8916-mtp.dtb \
-	$(LOCAL_DIR)/msm8916-qrd9-v1.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r01.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r03.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r04.dtb \
 	$(LOCAL_DIR)/msm8916-samsung-r06.dtb \
-	$(LOCAL_DIR)/msm8916-samsung-r08.dtb
+	$(LOCAL_DIR)/msm8916-samsung-r08.dtb \
+	$(LOCAL_DIR)/msm8916-samsung-j3ltespr.dtb
 endif
